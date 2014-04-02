99fa6e8ff76e5a051712a1d500fa835d2ac67f9d
tagger94 <tagger94@gmail.com>
2014-04-02 15:01:20 -0500

    CSV Unit File and tester
    Added CSV for unit importer and the unit tester

---
+++ src/Importer/UnitTester.java
@@ -0,0 +1,10 @@
+package Importer;
+
+public class UnitTester {
+
+	public static void main(String[] args) {
+		UnitImporter test = new UnitImporter();
+		test.getList();
+	}
+
+}
--- src/Importer/tester.java
+++ src/Importer/tester.java
@@ -5,8 +5,8 @@ public class tester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		UnitImporter test1 = new UnitImporter();
-		test1.getList();
+//		UnitImporter test1 = new UnitImporter();
+//		test1.getList();
 		
 		TerrainImporter test2 = new TerrainImporter();
 
