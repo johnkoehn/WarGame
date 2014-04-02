7e6d62724f07ffe7b8e848d2af4c111b8ba01daa
ShivaInu <omgsuiseiseki@gmail.com>
2014-04-02 15:29:16 -0500

    world class added

---
+++ src/Wargame/World.java
@@ -0,0 +1,14 @@
+package Wargame;
+
+public class World {
+//shell class
+
+/*Things that go here:
+ * Methods that deal with interaction between actors
+ * Methods that deal with interaction between actors and terrain
+ */
+	public World()
+	{}
+	
+
+}
