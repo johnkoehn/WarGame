b370d7defb0cd0adacff432443ae397ec3802df2
ShivaInu <omgsuiseiseki@gmail.com>
2014-04-09 15:01:57 -0500

    removed double terrain class

--- src/Wargame/Terrain.java
+++ src/Wargame/Terrain.java
@@ -102,39 +102,4 @@ public class Terrain {
 	public int getColor() {
 		return color;
 	}
-=======
-private int defense;
-private int id;
-private int subId;
-private int color;
-private boolean occupied;
-
-public Terrain(int defense, int id, int subId, int color)
-{this.defense = defense;
-this.id = id;
-this.subId = subId;
-this.color = color;
-this.occupied = false;
-}
-//access defense field
-public int getDefense()
-{return defense;}
-//access ID field
-public int getId()
-{return id;}
-//access subID field
-public int getSubId()
-{return subId;}
-//access color field
-public int getColor()
-{return color;}
-//returns whether or not the tile is occupied
-public boolean isOccupied()
-{return occupied;}
-//make the tile occupied
-public void makeOccupied()
-{occupied = true;}
-public void makeUnOccupied()
-{occupied = false;}
-
 }
