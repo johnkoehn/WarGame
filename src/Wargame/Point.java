b7b7a9df18f1e09919cd2a21c2634fa524b9722c
ShivaInu <omgsuiseiseki@gmail.com>
2014-04-09 18:21:06 -0500

    point class

---
+++ src/Wargame/Point.java
@@ -0,0 +1,19 @@
+package Wargame;
+
+public class Point {
+private float xpos;
+private float ypos;
+//construct a point using x and y values
+public Point(float xpos, float ypos)
+{this.xpos = xpos;
+this.ypos = ypos;
+}
+//return x value
+public float getX()
+{return xpos;}
+//return y value
+public float getY()
+{return ypos;}
+
+
+}
